39e2e646706abd8058b1e3b3c907f111ced7e207
nuandawm <derossi.giuliano@gmail.com>
2022-09-24 12:38:16 +0200

    add idea settings files to project

--- spa/src/app/store.ts
+++ spa/src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import playerCharacterCreationReduce from '../features/player-character-creation/playerCharacterCreationSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    playerCharacterCreation: playerCharacterCreationReduce
   },
 });
 
